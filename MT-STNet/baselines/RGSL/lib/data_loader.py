8a5ecd344f41b7701f866e60d32e176dbd36fc2c
GuojianZou <32932644+zouguojian@users.noreply.github.com>
2023-08-02 20:18:15 +0800

    first

---
+++ MT-STNet/baselines/RGSL/lib/data_loader.py
@@ -0,0 +1,75 @@
+import numpy as np
+import os
+
+class DataLoaderM(object):
+    def __init__(self, xs, ys, batch_size, pad_with_last_sample=True):
+        """
+        :param xs:
+        :param ys:
+        :param batch_size:
+        :param pad_with_last_sample: pad with the last sample to make number of samples divisible to batch_size.
+        """
+        self.batch_size = batch_size
+        self.current_ind = 0
+        if pad_with_last_sample:
+            num_padding = (batch_size - (len(xs) % batch_size)) % batch_size
+            x_padding = np.repeat(xs[-1:], num_padding, axis=0)
+            y_padding = np.repeat(ys[-1:], num_padding, axis=0)
+            xs = np.concatenate([xs, x_padding], axis=0)
+            ys = np.concatenate([ys, y_padding], axis=0)
+        self.size = len(xs)
+        self.num_batch = int(self.size // self.batch_size)
+        self.xs = xs
+        self.ys = ys
+
+    def shuffle(self):
+        permutation = np.random.permutation(self.size)
+        xs, ys = self.xs[permutation], self.ys[permutation]
+        self.xs = xs
+        self.ys = ys
+
+    def get_iterator(self):
+        self.current_ind = 0
+        def _wrapper():
+            while self.current_ind < self.num_batch:
+                start_ind = self.batch_size * self.current_ind
+                end_ind = min(self.size, self.batch_size * (self.current_ind + 1))
+                x_i = self.xs[start_ind: end_ind, ...]
+                y_i = self.ys[start_ind: end_ind, ...]
+                yield (x_i, y_i)
+                self.current_ind += 1
+
+        return _wrapper()
+
+class StandardScaler():
+    """
+    Standard the input
+    """
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+    def transform(self, data):
+        return (data - self.mean) / self.std
+    def inverse_transform(self, data):
+        return (data * self.std) + self.mean
+
+def get_load_dataset(dataset_dir, batch_size, valid_batch_size= None, test_batch_size=None):
+    data = {}
+    for category in ['train', 'val', 'test']:
+        cat_data = np.load(os.path.join(dataset_dir, category + '.npz'), allow_pickle=True)
+        data['x_' + category] = cat_data['x']
+        data['y_' + category] = cat_data['y']
+    scaler = StandardScaler(mean=data['x_train'][..., 0].mean(), std=data['x_train'][..., 0].std())
+    # Data format
+    for category in ['train', 'val', 'test']:
+        data['x_' + category][..., 0] = scaler.transform(data['x_' + category][..., 0])
+
+    data['train_loader'] = DataLoaderM(scaler.transform(data['x_train']), data['y_train'], batch_size)
+    data['val_loader'] = DataLoaderM(scaler.transform(data['x_val']), data['y_val'], valid_batch_size)
+    data['test_loader'] = DataLoaderM(scaler.transform(data['x_test']), data['y_test'], test_batch_size)
+    data['scaler'] = scaler
+    return data
+
+def load_adjacent(pickle_file):
+    adj_mx = np.load(pickle_file)['data']
+    return adj_mx
